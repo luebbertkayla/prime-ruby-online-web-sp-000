026456fc6a8cdb5579009849fe15208f60d9f4fc
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-04 00:41:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
-# Add  code here!
-# Prime numbers 
+# A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole number that can be divided evenly into another number.
+
 def prime?(number)
   start = 2
     return true 
