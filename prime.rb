a9ce57cd8112994a54bb9fc871a4fbb000a9d381
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-04 00:42:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
-# A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole number that can be divided evenly into another number. Another whole number multiplied by another whole number to create 
+# A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole number that can be divided evenly into another number. It is not 
 
 def prime?(number)
   start = 2
